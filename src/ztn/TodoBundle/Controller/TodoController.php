3201ec5f7576e07754ad1a4173f8b63fc0a46c9d
zouhaier <somaizouhaier@gmail.com>
2014-11-16 12:57:01 +0100

    création de projet

--- src/ztn/TodoBundle/Controller/TodoController.php
+++ src/ztn/TodoBundle/Controller/TodoController.php
@@ -11,9 +11,9 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 class TodoController extends Controller {
 
     public function indexAction() {
-        //o
         $request = $this->container->get('request');
         $event = new Event();
+        //utilisateur connécté
         $user = $this->getUser();
         $formulaire = $this->createForm(new CreateEventFormType(), $event);
         if ($request->getMethod() == 'POST') {
@@ -40,63 +40,70 @@ class TodoController extends Controller {
 
     public function deleteAction() {
         $request = $this->get('request');
-        if ($request->getMethod() == 'POST') {
+        //doit etre une requet ajax
+        if ($request->isXmlHttpRequest() && $request->getMethod() == 'POST') {
             $id = $request->request->get("id");
-        }
-        $list = $this->getDoctrine()->getManager()->getRepository('ztnTodoBundle:Event')->findOneBy(array(
-            'id' => $id
-        ));
+            $list = $this->getDoctrine()->getManager()->getRepository('ztnTodoBundle:Event')->findOneBy(array(
+                'id' => $id
+            ));
 
-        $this->getDoctrine()->getManager()->remove($list);
-        $this->getDoctrine()->getManager()->flush();
-        $this->get('session')->getFlashBag()->add(
+            $this->getDoctrine()->getManager()->remove($list);
+            $this->getDoctrine()->getManager()->flush();
+            $this->get('session')->getFlashBag()->add(
                     'success', 'la supression est reussi'
             );
+            return new JsonResponse(true);
+        }
 
-        return new JsonResponse(true);
+        return new JsonResponse(false);
     }
 
     public function editEtatAction() {
         $request = $this->get('request');
-        if ($request->getMethod() == 'POST') {
+        //doit etre une requet ajax
+        if ($request->isXmlHttpRequest() && $request->getMethod() == 'POST') {
             $id = $request->request->get("id");
-        }
-        $event = $this->getDoctrine()->getManager()->getRepository('ztnTodoBundle:Event')->findOneBy(array(
-            'id' => $id
-        ));
-        $event->setEtat(!$event->getEtat());
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($event);
-        $em->flush();
-        $this->get('session')->getFlashBag()->add(
+
+            $event = $this->getDoctrine()->getManager()->getRepository('ztnTodoBundle:Event')->findOneBy(array(
+                'id' => $id
+            ));
+            $event->setEtat(!$event->getEtat());
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($event);
+            $em->flush();
+            $this->get('session')->getFlashBag()->add(
                     'success', 'la modification est reussi'
             );
-        return new JsonResponse(true);
+            return new JsonResponse(true);
+        }
+        return new JsonResponse(false);
     }
 
     public function editAction() {
         $request = $this->get('request');
         $user = $this->getUser();
-        if ($request->getMethod() == 'POST') {
+        //doit etre une requet ajax
+        if ($request->isXmlHttpRequest() && $request->getMethod() == 'POST') {
             $parametre = $request->request->all();
             $keys = array_keys($parametre);
             if (is_array($keys)) {
                 $id = $keys[0];
             }
+            else
+                $id = 0;
             $name = $parametre[$id];
             $event = $this->getDoctrine()->getManager()->getRepository('ztnTodoBundle:Event')->findOneBy(array(
                 'id' => $id
             ));
-
             $event->setName($name);
             $em = $this->getDoctrine()->getManager();
             $em->persist($event);
             $em->flush();
-
             $this->get('session')->getFlashBag()->add(
                     'success', 'la modification est reussi'
             );
         }
+        //redirection vers l'action indexAction pour récupérer les events et afficher la page
         return $this->redirect($this->generateUrl('ztn_todo_homepage'));
     }
 
